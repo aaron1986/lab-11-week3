11bb016fe5aa2966c9279f02fe5c03dfe2562d58
aaron1986 <aaronbrucesmith86@gmail.com>
2023-10-05 12:56:14 +0100

    code not working yet

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // Dom nodes
 let imageContainer = document.getElementById("section-images");
-
-// I wanted to use this code so I can write new ways of writing code
 let image1 = document.querySelector("section img:first-child");
 let image2 = document.querySelector("section img:nth-child(2)");
 let image3 = document.querySelector("section img:last-child");
@@ -10,20 +8,24 @@ let image3 = document.querySelector("section img:last-child");
 let rounds = 0;
 const maxRounds = 25;
 
-
-const imagesArray = [];
+const products = [];
 
 function Images(name, src, views, clicks) {
     this.name = name;
     this.src = src;
     this.views = views;
     this.clicks = clicks;
-
-
-    imagesArray.push(this);
+    products.push(this);
 }
 
-if(localStorage.getItem("imagesArray") === null) {
+// Check if data exists in localStorage
+const storedProducts = localStorage.getItem("products");
+
+if (storedProducts) {
+    // Data exists in localStorage, parse and use it
+    const parsedProducts = JSON.parse(storedProducts);
+    products.push(...parsedProducts);
+} else {
     new Images("Unspecificed Droid Bag", "./img/bag.jpg", 0,0);
     new Images("Fraudulent Banana Slicer", "./img/banana.jpg", 0,0);
     new Images("iWiper", "./img/bathroom.jpg", 0,0);
@@ -43,127 +45,67 @@ if(localStorage.getItem("imagesArray") === null) {
     new Images("Tinned Unicorn Meat", "./img/unicorn.jpg", 0,0);
     new Images("Infinite Watering Can", "./img/water-can.jpg", 0,0);
     new Images("Why?-n Glass", "./img/wine-glass.jpg", 0,0);
-} else {
-    const imagesArray = JSON.parse(localStorage.getItem("products"));
-    for(let i = 0; i < imagesArray.length; i++) {
-        new Images(imagesArray[i].name, imagesArray[i].views, imagesArray[i].clicks);
-
-    }
 }
 
-// Function to choose a random image
 function getRandomIndex() {
-    return Math.floor(Math.random() * imagesArray.length);
+    return Math.floor(Math.random() * products.length);
 }
 
 function renderImages() {
-    // get 2 random indexes from the images array
     let img1Index = getRandomIndex();
     let img2Index = getRandomIndex();
     let img3Index = getRandomIndex();
 
-    // prevent the two images being the same goat
     while (img1Index === img2Index || img1Index === img3Index || img2Index === img3Index) {
         img2Index = getRandomIndex();
         img3Index = getRandomIndex();
     }
 
-    // change the src of our 3 images
-    image1.src = imagesArray[img1Index].src;
-    image2.src = imagesArray[img2Index].src;
-    image3.src = imagesArray[img3Index].src;
+    image1.src = products[img1Index].src;
+    image2.src = products[img2Index].src;
+    image3.src = products[img3Index].src;
 
-    // change name of our 3 images
-    image1.alt = imagesArray[img1Index].name;
-    image2.alt = imagesArray[img2Index].name;
-    image3.alt = imagesArray[img3Index].name;
+    image1.alt = products[img1Index].name;
+    image2.alt = products[img2Index].name;
+    image3.alt = products[img3Index].name;
 
-    // increase the image views
-    imagesArray[img1Index].views++;
-    imagesArray[img2Index].views++;
-    imagesArray[img3Index].views++;
-
-    viewCounts[img1Index]++;
-    viewCounts[img2Index]++;
-    viewCounts[img3Index]++;
+    products[img1Index].views++;
+    products[img2Index].views++;
+    products[img3Index].views++;
 }
 
-
-
-// Create an array to store click counts
 const viewCounts = [];
-for(let i = 0; i < imagesArray.length; i++) {
+for (let i = 0; i < products.length; i++) {
     viewCounts.push(0);
 }
 
-// Create an array to store click counts
 const clickCounts = [];
-for(let i = 0; i < imagesArray.length; i++) {
+for (let i = 0; i < products.length; i++) {
     clickCounts.push(0);
 }
 
-// Create an array to store image names for the chart labels
-const chartLabels = [];
-for(let i = 0; i < imagesArray.length; i++) {
-  chartLabels.push(imagesArray[i].name);
-}
+image1.addEventListener("click", handleImageClick);
+image2.addEventListener("click", handleImageClick);
+image3.addEventListener("click", handleImageClick);
 
-
-//Canvas for the chart
-const chartCanvas = document.getElementById("chart");
-const chartContext = chartCanvas.getContext("2d");
-
-// Create the chart
-const chart = new Chart(chartContext, {
-    type: "bar",
-    data: {
-        labels: chartLabels,
-        datasets: [{
-            label: "Clicks",
-            data: clickCounts,
-            backgroundColor: ["rgba(39, 172, 245, 0.8)", "rgba(245, 40, 145, 0.8)", "rgba(39, 245, 193, 0.8)", "rgba(241, 245, 39, 0.8)", "rgba(245, 133, 39, 0.8)"],
-        },
-        {
-            label: "Views",
-            data: viewCounts,
-            backgroundColor: ["#000"],
-        },
-    ],
-    },
-});
-
-// Modified handleImageClick function
 function handleImageClick(event) {
     if (rounds >= maxRounds) {
         alert("You have completed all rounds.");
-        localStorage.setItem("imagesArray", JSON.stringify(imagesArray));
         return;
     }
 
-    // increase the number of times the user has clicked
     rounds++;
-
-    // Get the name of the image clicked
     let clickedImage = event.target.alt;
 
-    for (let i = 0; i < imagesArray.length; i++) {
-        if (clickedImage === imagesArray[i].name) {
+    for (let i = 0; i < products.length; i++) {
+        if (clickedImage === products[i].name) {
             clickCounts[i]++;
             break;
         }
     }
 
-    // Update the chart with new data
-    chart.update();
-
-    // Render more images
+    localStorage.setItem("products", JSON.stringify(products));
     renderImages();
 }
 
-// Add event listener for image clicks
-image1.addEventListener("click", handleImageClick);
-image2.addEventListener("click", handleImageClick);
-image3.addEventListener("click", handleImageClick);
-
-// Render initial images
 renderImages();
--- chart.js
+++ chart.js
@@ -1,28 +1,42 @@
-const resultButton = document.getElementById('resultButton')
+/* const imagesArrayString = JSON.stringify(imagesArray);
+localStorage.setItem("Images", imagesArrayString);
 
-const handleImageClick = function() {
-    const images = localStorage.getItem('images')
-    const parseImages = JSON.parse(images)
-    console.log(parsedImages);
+const retreieveData = JSON.parse(localStorage.getItem("images"));
 
-    for (let i = 0; i < parseImages.length; i++) {
-        const newCat = new Cat(parseImages[i].name)
-        state.allImages.push()
-        new Images.render()
-    }
+for(let i = 0; i < localStorage.length; i++) {
+    console.log(localStorage[i]);
+} */
 
+
+const chartCanvas = document.getElementById("chart");
+const chartContext = chartCanvas.getContext("2d");
+
+const products = JSON.parse(localStorage.getItem("products"));
+
+const chartLabels = [];
+const clickCounts = [];
+
+for (let i = 0; i < products.length; i++) {
+    chartLabels.push(products[i].name);
+    clickCounts.push(products[i].clicks);
 }
 
-resultButton.addEventListener('click', handleButtonClick)
-
-function handleSubmit(event) {
-
-    event.preventDefault();
-    
-    const newImg  = new Images(imagesArray.value)
-    imagesArray.allImages.push(newImg);
-    imgForm.reset();
-    newImg.render();
-}
-
-newImg.addEventListener('submit', handleSubmit);
+const chart = new Chart(chartContext, {
+    type: "bar",
+    data: {
+        labels: chartLabels,
+        datasets: [
+            {
+                label: "Clicks",
+                data: clickCounts,
+                backgroundColor: [
+                    "rgba(39, 172, 245, 0.8)",
+                    "rgba(245, 40, 145, 0.8)",
+                    "rgba(39, 245, 193, 0.8)",
+                    "rgba(241, 245, 39, 0.8)",
+                    "rgba(245, 133, 39, 0.8)"
+                ],
+            },
+        ],
+    },
+});
